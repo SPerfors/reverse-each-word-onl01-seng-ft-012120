d5f611f4f1c667a6e6701ecd939c5a3a7a65664e
Steven Perfors <stevecanbuild@gmail.com>
2020-01-25 21:25:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,6 +4,10 @@
   #new_array.join(" ")
 #end
 
-def reverse_each_word_collect(sentence)
+def reverse_each_word(sentence)
+  new_array = array.split(" ")
+  reverse_array = new_array.collect {|x| x.reverse!}
+  new_array.join(" ")
+end
   
 reverse_each_word("Hello there and how are you?")
